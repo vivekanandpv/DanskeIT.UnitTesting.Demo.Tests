b5afb0097b111d2b49ec595e6847d3d1a2494d75
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-14 15:44:54 +0530

    All added

---
+++ CustomersControllerShould.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DanskeIT.UnitTesting.Demo.Controllers;
+using DanskeIT.UnitTesting.Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace DanskeIT.UnitTesting.Demo.Tests
+{
+    public class CustomersControllerShould
+    {
+        [Test]
+        public void ReturnListOfCustomers()
+        {
+            //  arrange
+            var controller = new CustomersController();
+
+            //  act
+            var result = controller.Get();
+            var customers = result.Result;
+
+            Assert.NotNull(customers);
+        }
+
+        
+        [Test]
+        public void ReturnCustomerById()
+        {
+            //  arrange
+            var controller = new CustomersController();
+
+            //  act
+            var result = controller.Get(123);
+            var customer = result.Result;
+
+            Assert.NotNull(customer);
+        }
+
+        [Test]
+        public void CreateCustomer()
+        {
+            //  arrange
+            var controller = new CustomersController();
+
+            //  act
+            var customer = new Customer();
+            var result = controller.Create(customer) as OkObjectResult;
+            
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void UpdateCustomer()
+        {
+            //  arrange
+            var controller = new CustomersController();
+
+            //  act
+            var customer = new Customer();
+            var result = controller.Update(customer, 123) as OkObjectResult;
+            
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void SuccessfullyDeleteCustomerWithIdLessThan100()
+        {
+            //  arrange
+            var controller = new CustomersController();
+
+            //  act
+            var result = controller.Delete(98) as OkObjectResult;
+            
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void PreventDeletionOfCustomerWithIdGreaterThanOrEqualTo100()
+        {
+            //  arrange
+            var controller = new CustomersController();
+
+            //  act
+            var result = controller.Delete(123) as NotFoundResult;
+            
+
+            Assert.NotNull(result);
+        }
+    }
+}
